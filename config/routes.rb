1f681338e02d8cf4d02d753c240a7a0a9dc448a2
Ramesh K <rameshkror@gmail.com>
2014-12-31 04:41:17 +0530

    Final Commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :twitter
   get '/tweets' => "twitter#tweets"
-  #get '/oauth_account' => "twitter#oauth_account"
   get '/twitter_oauth_url' => 'twitter#generate_twitter_oauth_url'
 
   root :to => "twitter#index"
